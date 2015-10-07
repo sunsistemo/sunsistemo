532801573511104b0076d5b0a9ccff9f6ea0c9bb
rahiel <rahiel@protonmail.ch>
2015-10-07 10:31:44 +0200

    add some lights

--- sunsistemo.es6.js
+++ sunsistemo.es6.js
@@ -22,13 +22,21 @@ function init() {
     let spheres = [];
     for (let b of bodies) {
         let geometry = new THREE.SphereGeometry(20, 32, 32);
-        let material = new THREE.MeshNormalMaterial();
+        let material = new THREE.MeshPhongMaterial();
         let sphere = new THREE.Mesh(geometry, material);
 
         sphere.position.set(b.r.x, b.r.y, b.r.z);
         scene.add(sphere);
         spheres.push(sphere);
     }
+
+    // sun
+    let light = new THREE.PointLight(0xffffff);
+    spheres[0].add(light);
+
+    // overall light
+    let ambient = new THREE.AmbientLight( 0x404040 );
+    scene.add(ambient);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0x000000);
@@ -47,7 +55,7 @@ function init() {
 
 
 function animate() {
-    bodies = calc.leapfrog(bodies, 0.001);
+    bodies = calc.symplectic_euler(bodies, 0.001);
 
     for (let i = 0; i < bodies.length; i++) {
         let pos = bodies[i].r;
